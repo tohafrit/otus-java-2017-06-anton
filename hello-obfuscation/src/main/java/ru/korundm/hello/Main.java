bccd9742057855742fdefa69987c952382e2050e
anton <tohafrit@gmail.com>
2017-06-07 11:40:25 +0300

    тест

--- hello-obfuscation/src/main/java/ru/korundm/hello/Main.java
+++ hello-obfuscation/src/main/java/ru/korundm/hello/Main.java
@@ -1,9 +1,10 @@
 package ru.korundm.hello;
 
 /**
- * Created by pakhunov_an on 07.06.2017.
+ * Created by anton on 07.06.2017.
  */
 public class Main {
-
-
+    public static void main(String[] args) {
+        System.out.println( "Hello world!" );
+    }
 }
